7a2a0e4bf525464c566921975f87775479d7d828
HeilOliver <ohe2033@students.fhv.at>
2018-03-22 18:45:25 +0100

    Add GuestDomain

---
+++ Codebase/Roomix/domain/guest-domain/src/test/java/at/fhv/roomix/domain/guest/GuestDomainMock.java
@@ -0,0 +1,12 @@
+package at.fhv.roomix.domain.guest;
+
+/**
+ * Roomix
+ * at.fhv.roomix.domain.guest
+ * GuestDomainMock
+ * 22/03/2018 Oliver
+ * <p>
+ * Mock for IGuestDomain
+ */
+public class GuestDomainMock implements IGuestDomain {
+}
