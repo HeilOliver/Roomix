08e1433939aca922b8442d2fe9542612ec8d6ab4
sge <25079878+sge5641@users.noreply.github.com>
2018-03-22 18:47:41 +0100

    add guest controller

---
+++ Codebase/Roomix/controller/guest-controller/src/main/java/at/fhv/roomix/controller/guest/model/GuestPojo.java
@@ -0,0 +1,14 @@
+package at.fhv.roomix.controller.guest.model;
+
+/**
+ * Roomix
+ * at.fhv.roomix.controller.guest.model
+ * GuestPojo
+ * 22.03.2018 sge
+ *
+ * The Guest Pojo which is been used later on
+ */
+public class GuestPojo {
+
+
+}
---
+++ Codebase/Roomix/controller/guest-controller/src/test/java/at/fhv/roomix/controller/guest/GuestControllerFactoryTest.java
@@ -0,0 +1,13 @@
+package at.fhv.roomix.controller.guest;
+
+/**
+ * Roomix
+ * at.fhv.roomix.controller.guest
+ * GuestControllerFactoryTest
+ * 22.03.2018 sge
+ *
+ *
+ */
+class GuestControllerFactoryTest {
+
+}
