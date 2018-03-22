5b62d73660196bfb93e0d43149a91e95ab2cc629
sge <25079878+sge5641@users.noreply.github.com>
2018-03-22 19:03:02 +0100

    added tests to guestController

--- Codebase/Roomix/controller/guest-controller/src/test/java/at/fhv/roomix/controller/guest/GuestControllerFactoryTest.java
+++ Codebase/Roomix/controller/guest-controller/src/test/java/at/fhv/roomix/controller/guest/GuestControllerFactoryTest.java
@@ -1,5 +1,9 @@
 package at.fhv.roomix.controller.guest;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Roomix
  * at.fhv.roomix.controller.guest
@@ -9,5 +13,41 @@ package at.fhv.roomix.controller.guest;
  *
  */
 class GuestControllerFactoryTest {
+
+    @Test
+    void injectInFactory_null() {
+        GuestControllerMock guestControllerMock = new GuestControllerMock();
+
+        IGuestController guestController = GuestControllerFactory.GetInstance();
+        assertNotEquals(guestController, guestControllerMock);
+
+        GuestControllerFactory.InjectDependency(null);
+        IGuestController newGuestController = GuestControllerFactory.GetInstance();
+        assertNotEquals(guestController, newGuestController);
+        assertNotEquals(guestControllerMock, newGuestController);
+    }
+
+    @Test
+    void getInstance_get() {
+        IGuestController guestController = GuestControllerFactory.GetInstance();
+        assertNotNull(guestController);
+    }
+
+    @Test
+    void getInstance_type() {
+        IGuestController guestController = GuestControllerFactory.GetInstance();
+        assertTrue(guestController instanceof GuestController);
+    }
+
+    @Test
+    void injectInFactory_ok() {
+        GuestControllerMock guestControllerMock = new GuestControllerMock();
+        GuestControllerFactory.InjectDependency(guestControllerMock);
+
+        IGuestController iGuestController = GuestControllerFactory.GetInstance();
+
+        assertEquals(iGuestController, guestControllerMock);
+    }
+
 
 }
---
+++ Codebase/Roomix/controller/guest-controller/src/test/java/at/fhv/roomix/controller/guest/GuestControllerMock.java
@@ -0,0 +1,14 @@
+package at.fhv.roomix.controller.guest;
+
+/**
+ * Roomix
+ * at.fhv.roomix.controller.guest
+ * GuestControllerMock
+ * 22.03.2018 sge
+ *
+ * Mock fpr IGuestController
+ */
+public class GuestControllerMock implements IGuestController {
+
+
+}
