f243ecc5d10e250a1a1f7ade44294f683ca047c3
Robert Schmitzer <36699190+RobertSchmitzer@users.noreply.github.com>
2018-04-05 18:54:42 +0200

    Dao für Reservierung und ContractingParty erstellt!
    -ReservierungsDao erstellt
    -Dao für ContractingParty erstellt (wird möglicherweise nicht einmal benötigt)

--- Codebase/Roomix/persist/src/main/java/at/fhv/roomix/persist/ContractingPartyDao.java
+++ Codebase/Roomix/persist/src/main/java/at/fhv/roomix/persist/ContractingPartyDao.java
@@ -1,6 +1,7 @@
 package at.fhv.roomix.persist;
 
 import at.fhv.roomix.persist.model.ContractingpartyEntity;
+import org.hibernate.HibernateException;
 
 public class ContractingPartyDao extends AbstractDao<ContractingpartyEntity, Integer> {
 
@@ -8,5 +9,17 @@ public class ContractingPartyDao extends AbstractDao<ContractingpartyEntity, Int
         super(ContractingpartyEntity.class);
     }
 
+    public ContractingPartyDao getInstance(){
+        return new ContractingPartyDao();
+    }
 
+    @Override
+    protected void internalSave(ContractingpartyEntity entity) throws HibernateException {
+        session.beginTransaction();
+        session.saveOrUpdate(entity.getContactByContact());
+        session.saveOrUpdate(entity.getPartneragreementsByContractingPartyId());
+        session.saveOrUpdate(entity.getReservationsByContractingPartyId());
+        session.save(entity);
+        session.getTransaction().commit();
+    }
 }
