90b9d0737adb1ff0db87f842c4d3c6e34c837b59
HeilOliver <ohe2033@students.fhv.at>
2018-04-08 21:44:18 +0200

    Add SessionID to calls

---
+++ Codebase/Roomix/ui/src/main/java/at/fhv/roomix/ui/config/SessionProvider.java
@@ -0,0 +1,39 @@
+package at.fhv.roomix.ui.config;
+
+/**
+ * Roomix
+ * at.fhv.roomix.ui.config
+ * SessionProvider
+ * 08/04/2018 Oliver
+ * <p>
+ * Enter Description here
+ */
+public class SessionProvider {
+
+    private static final Object lock = new Object();
+    private static SessionProvider instance;
+
+        private SessionProvider() {
+
+    }
+
+    public static SessionProvider getInstance() {
+        if (instance != null) return instance;
+        synchronized (lock) {
+            if (instance == null) {
+                instance = new SessionProvider();
+            }
+        }
+        return instance;
+    }
+
+    private static long sessionId;
+
+    public static long getSessionId() {
+        return sessionId;
+    }
+
+    public static void setSessionId(long sessionId) {
+        SessionProvider.sessionId = sessionId;
+    }
+}
