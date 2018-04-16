984ded957709882946454109c37679096e8a3db5
HeilOliver <ohe2033@students.fhv.at>
2018-04-17 00:39:53 +0200

    Add Reservation View

--- Codebase/Roomix/ui/src/main/java/at/fhv/roomix/ui/view/reservation/ReservationView.java
+++ Codebase/Roomix/ui/src/main/java/at/fhv/roomix/ui/view/reservation/ReservationView.java
@@ -1,7 +1,10 @@
 package at.fhv.roomix.ui.view.reservation;
 
+import at.fhv.roomix.ui.view.contact.ContactViewModel;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Roomix
@@ -12,7 +15,15 @@ import de.saxsys.mvvmfx.InjectViewModel;
  * Enter Description here
  */
 public class ReservationView implements FxmlView<ReservationViewModel> {
-
     @InjectViewModel
     private ReservationViewModel viewModel;
+    @FXML
+    private AnchorPane editView;
+    @FXML
+    private AnchorPane contentView;
+
+    public void initialize() {
+        editView.visibleProperty().bind(viewModel.editViewVisibleProperty());
+        contentView.visibleProperty().bind(viewModel.contentViewVisibleProperty());
+    }
 }
--- Codebase/Roomix/ui/src/main/java/at/fhv/roomix/ui/view/reservation/ReservationViewModel.java
+++ Codebase/Roomix/ui/src/main/java/at/fhv/roomix/ui/view/reservation/ReservationViewModel.java
@@ -1,9 +1,13 @@
 package at.fhv.roomix.ui.view.reservation;
 
+import at.fhv.roomix.ui.view.contact.scopes.ContactViewScope;
 import at.fhv.roomix.ui.view.reservation.scope.ReservationViewScope;
 import com.mysql.cj.api.x.View;
+import de.saxsys.mvvmfx.InjectScope;
 import de.saxsys.mvvmfx.ScopeProvider;
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 
 /**
  * Roomix
@@ -15,4 +19,34 @@ import de.saxsys.mvvmfx.ViewModel;
  */
 @ScopeProvider(scopes = ReservationViewScope.class)
 public class ReservationViewModel implements ViewModel {
+
+    private BooleanProperty contentViewVisible = new SimpleBooleanProperty();
+    private BooleanProperty editViewVisible = new SimpleBooleanProperty();
+
+    @InjectScope
+    private ReservationViewScope viewScope;
+
+    public void initialize() {
+        viewScope.subscribe(ContactViewScope.commandContentView, (key, payload) -> showContent());
+        viewScope.subscribe(ContactViewScope.commandEditView, (key, payload) -> showEdit());
+        showContent();
+    }
+
+    private void showContent() {
+        contentViewVisible.setValue(true);
+        editViewVisible.setValue(false);
+    }
+
+    private void showEdit() {
+        contentViewVisible.setValue(false);
+        editViewVisible.setValue(true);
+    }
+
+    BooleanProperty contentViewVisibleProperty() {
+        return contentViewVisible;
+    }
+
+    BooleanProperty editViewVisibleProperty() {
+        return editViewVisible;
+    }
 }
