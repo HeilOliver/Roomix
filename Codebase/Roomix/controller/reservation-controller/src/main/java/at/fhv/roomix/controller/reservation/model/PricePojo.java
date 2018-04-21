6f04abec3ce2710b50f918cff302f51e226cc856
Robert Schmitzer <36699190+RobertSchmitzer@users.noreply.github.com>
2018-04-21 16:22:35 +0200

    PricePojo added
    added price pojo

---
+++ Codebase/Roomix/controller/reservation-controller/src/main/java/at/fhv/roomix/controller/reservation/model/PricePojo.java
@@ -0,0 +1,15 @@
+package at.fhv.roomix.controller.reservation.model;
+
+public class PricePojo {
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private int price;
+
+
+}
