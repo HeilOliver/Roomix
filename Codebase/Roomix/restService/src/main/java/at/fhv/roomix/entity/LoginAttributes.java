17bebc64a02653521410994e8bcbf22a3608f42b
Robert Schmitzer <36699190+RobertSchmitzer@users.noreply.github.com>
2018-05-14 08:37:58 +0200

    Database added / Spring implementation im Pom
    Datenbank wurde hinzugefügt (H2) inklusive der Spring anfügung im pom.
    Maven Modul erstellt!

---
+++ Codebase/Roomix/restService/src/main/java/at/fhv/roomix/entity/LoginAttributes.java
@@ -0,0 +1,44 @@
+package at.fhv.roomix.entity;
+
+import javax.persistence.*;
+
+/**
+ * Building Attributes from the Sites into Java variables!
+ * These Attributes and the Table itself are not existing, so if we start the application, the H2 Database draws automaticly the missing table!
+ */
+@Entity
+@Table(name = "Login")
+public class LoginAttributes {
+    private int id;
+    private String pw;
+    private String userid;
+
+    @Id
+    @Column(name = "userID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Basic
+    @Column(name = "username")
+    public String getUserid() {
+        return userid;
+    }
+    public void setUserid(String entry) {
+        this.userid = entry;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPw() {
+        return pw;
+    }
+    public void setPw(String entry) {
+        this.pw = entry;
+    }
+}
