fec32b525818610afd9f07d89f3a7947a7d7dd7b
Robert Schmitzer <36699190+RobertSchmitzer@users.noreply.github.com>
2018-06-03 18:43:24 +0200

    Dependency Controller

--- Codebase/Roomix/webLogin/src/main/java/at/fhv.roomix.webLogin/security/controller/ReservationCheckRestController.java
+++ Codebase/Roomix/webLogin/src/main/java/at/fhv.roomix.webLogin/security/controller/ReservationCheckRestController.java
@@ -1,5 +1,7 @@
 package at.fhv.roomix.webLogin.security.controller;
 
+import at.fhv.roomix.controller.implement.reservationcontroller.ReservationController;
+import at.fhv.roomix.controller.model.RoomCategoryPojo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/ReservationCheck")
@@ -17,6 +20,9 @@ public class ReservationCheckRestController {
         //convert String to LocalDate
         LocalDate startDate = LocalDate.parse(getStartDate, formatter);
         LocalDate endDate = LocalDate.parse(getEndDate, formatter);
+        Collection roomCategoryPojoCollection = new Collection<RoomCategoryPojo>;
+        ReservationController reservationController = new ReservationController();
+
 
 
 
