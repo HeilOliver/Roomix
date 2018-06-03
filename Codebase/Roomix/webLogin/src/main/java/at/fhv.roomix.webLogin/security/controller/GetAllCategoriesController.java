2b0cf2aa5bf2e0fcf5988001b016e09df8ef077a
Robert Schmitzer <36699190+RobertSchmitzer@users.noreply.github.com>
2018-06-03 17:51:31 +0200

    Added some controller

---
+++ Codebase/Roomix/webLogin/src/main/java/at/fhv.roomix.webLogin/security/controller/GetAllCategoriesController.java
@@ -0,0 +1,9 @@
+package at.fhv.roomix.webLogin.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/AllCategories")
+public class GetAllCategoriesController {
+}
---
+++ Codebase/Roomix/webLogin/src/main/java/at/fhv.roomix.webLogin/security/controller/ReservationCheckRestController.java
@@ -0,0 +1,24 @@
+package at.fhv.roomix.webLogin.security.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/ReservationCheck")
+public class ReservationCheckRestController {
+    @RequestMapping(method = RequestMethod.POST)
+    public String getSaveContact(@RequestParam(value="startDate") String getStartDate,@RequestParam(value="endDate") String getEndDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
+        //convert String to LocalDate
+        LocalDate startDate = LocalDate.parse(getStartDate, formatter);
+        LocalDate endDate = LocalDate.parse(getEndDate, formatter);
+
+    return "Test";
+    }
+}
